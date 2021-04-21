c9fbf25096bdbc5969cac2203de2e7e22ef93a0d
GioOli7 <gio07live@gmail.com>
2021-04-21 14:52:50 +0200

    next-prev slider buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 const app = new Vue({
     el: "#app",
     data: {
-        
+        images: [
+            './images/image1.jpg',
+            './images/image2.jpg',
+            './images/image3.jpg',
+            './images/image4.jpg',
+            './images/image5.jpg',
+            './images/image6.jpg'
+        ],
+        imageIndex: 0,
+    },
+    methods: {
+        next() {
+            this.imageIndex++;
+            if (this.imageIndex > this.images.length - 1) {
+                this.imageIndex = 0;
+            }
+        },
+        prev() {
+            this.imageIndex--;
+            if (this.imageIndex < 0) {
+                this.imageIndex = this.images.length - 1;
+            }
+        }
     }
 })
